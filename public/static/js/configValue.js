9814e4687729deffbd10550a0fd34da918ca8126
Erika Assis <erika.s.assis@gmail.com>
2016-12-22 10:27:23 -0200

    Alterado configValue.js

--- public/static/js/configValue.js
+++ public/static/js/configValue.js
@@ -1,8 +1,9 @@
-
 /*
 	Constantes de configuraçao do programa.
 */
 angular.module('BomCodigo').value('config', {
+	//baseURL: 'http://127.0.0.1:7000/',
     baseURL: 'https://tbwebservice.herokuapp.com',
-    baseURLGIT: 'https://api.github.com'
+    baseURLGIT: 'https://api.github.com',
+    tokenPadrao: '?access_token=....',
 });
